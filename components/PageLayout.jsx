df607af725d072e1af7040663941e084ba53de9c
RodriMR <rodrimr2008@gmail.com>
2022-11-14 19:15:56 -0300

    fetch and basic styling

---
+++ components/PageLayout.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Head from "next/head";
+import Link from "next/link";
+export default function PageLayout({ children, title }) {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta
+          name="description"
+          content="newsapp - the best app to read news"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header>
+        <div>
+          <Link href="/">newsapi</Link>
+        </div>
+        <div>
+          <Link href="/about">About</Link>
+        </div>
+      </header>
+      <main>{children}</main>
+      <style jsx>
+        {`
+          header {
+            display: flex;
+            justify-content: space-between;
+            padding: 20px;
+          }
+        `}
+      </style>
+    </>
+  );
+}
---
+++ pages/article/[id].js
@@ -0,0 +1,3 @@
+export default function Article() {
+  return <div>Esto es un articulo</div>;
+}
